7bf1e0beb33455e8c8d04e6bd15bd7bf2af956a5
sgalella <santi.galella@gmail.com>
2020-05-11 13:37:54 +0200

    Update correlation data

--- StatisticalTest/chiFig5b.m
+++ StatisticalTest/chiFig5b.m
@@ -3,8 +3,8 @@ clc;
 clear;
 
 % Load data
-interSeriesCorr = csvread("fig5_interseries_corr.csv");
-intraSeriesCorr = csvread("fig5_intraseries_corr.csv");
+interSeriesCorr = csvread("fig5_interseries_cor.csv");
+intraSeriesCorr = csvread("fig5_intraseries_cor.csv");
 
 % Compute the chi-square test
 nbins = 200;
