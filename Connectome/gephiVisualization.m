cce09a5d54ebbc761d828eccd008a606dfd1145b
sgalella <santi.galella@gmail.com>
2020-05-17 13:10:22 +0200

    Clean and comment code

--- Connectome/gephiVisualization.m
+++ Connectome/gephiVisualization.m
@@ -1,100 +1,105 @@
-clear; clc;
+% Clear command window and workspace
+clear;
+clc;
 
-addpath('Data/')
+% Add path to dependencies
+addpath 'Data/'
 
+% Load connectome data
 load('Connectome_full.mat')
-
 neuronsGephi = neurons;
-for i = 1:length(neurons)
-    if any(strcmp(reservoirNeurons,string(neurons(i))))
-        neuronsGephi(i,2) = {'Reservoir'};
-    elseif any(strcmp(inputLayer{1,1},string(neurons(i))))
-        neuronsGephi(i,2) = {'Input'};
+nNeurons = length(neuronsGephi);
+
+% Classify each neuron according their position in the reservoir computing
+% architecture
+for iNeuron = 1:nNeurons
+    if any(strcmp(reservoirNeurons,string(neurons(iNeuron))))
+        neuronsGephi(iNeuron, 2) = {'Reservoir'};
+    elseif any(strcmp(inputLayer{1,1},string(neurons(iNeuron))))
+        neuronsGephi(iNeuron ,2) = {'Input'};
     else
-        neuronsGephi(i,2) = {'Readout'};
+        neuronsGephi(iNeuron ,2) = {'Readout'};
     end
 end
 
-for i = 1:length(neurons)
-    if i < 84
-        neuronsGephi(i,3) = {'Sensory Neurons'};
-    elseif i >= 84 && i < 165
-        neuronsGephi(i,3) = {'Interneurons'};
-    elseif i >= 165 && i < 273
-        neuronsGephi(i,3) = {'Motor Neurons'};
-    elseif i >= 273 && i < 368
-        neuronsGephi(i,3) = {'Bodywall Muscles'};
-    elseif i >= 368 && i < 384
-        neuronsGephi(i,3) = {'Other-end Organs'};
-    elseif i >= 384
-        neuronsGephi(i,3) = {'Sex-specific Cells'};
+% Classify each neuron according to their types
+for iNeuron = 1:nNeurons
+    if iNeuron < 84
+        neuronsGephi(iNeuron, 3) = {'Sensory Neurons'};
+    elseif iNeuron >= 84 && iNeuron < 165
+        neuronsGephi(iNeuron, 3) = {'Interneurons'};
+    elseif iNeuron >= 165 && iNeuron < 273
+        neuronsGephi(iNeuron, 3) = {'Motor Neurons'};
+    elseif iNeuron >= 273 && iNeuron < 368
+        neuronsGephi(iNeuron, 3) = {'Bodywall Muscles'};
+    elseif iNeuron >= 368 && iNeuron < 384
+        neuronsGephi(iNeuron, 3) = {'Other-end Organs'};
+    elseif iNeuron >= 384
+        neuronsGephi(iNeuron, 3) = {'Sex-specific Cells'};
     end
 end
 
+% Save neurons info
 fileID = fopen('Gephi/neurons.csv','w');
 fprintf(fileID,'Id, label, posx, posy, nodetype, function\n');
 
-
+% Store position
 input_posy = [-0.5,0.5];
 readout_posy = [-0.5,0,0.5];
-
 knownNeurons = [15 16 129 159 157 143 144 150 169];
-
-for i = 1:length(neuronsGephi)
-    if any(knownNeurons == i)
-        if strcmp(neuronsGephi(i,2),'Reservoir')
-            fprintf(fileID,'%s,%s,%f,%f,%s,%s\n',string(neuronsGephi(i,1)),string(neuronsGephi(i,1)),...
-                8+normrnd(0,1.5),normrnd(0,1.5),string(neuronsGephi(i,2)),string(neuronsGephi(i,3)));
-        elseif strcmp(neuronsGephi(i,2),'Input')
-            fprintf(fileID,'%s,%s,%f,%f,%s,%s\n',string(neuronsGephi(i,1)),string(neuronsGephi(i,1)),...
-                0,input_posy(1),string(neuronsGephi(i,2)),string(neuronsGephi(i,3)));
+for iNeuron = 1:nNeurons
+    if any(knownNeurons == iNeuron)
+        if strcmp(neuronsGephi(iNeuron, 2),'Reservoir')
+            fprintf(fileID,'%s,%s,%f,%f,%s,%s\n',string(neuronsGephi(iNeuron, 1)),string(neuronsGephi(iNeuron, 1)),...
+                8+normrnd(0, 1.5),normrnd(0, 1.5),string(neuronsGephi(iNeuron, 2)),string(neuronsGephi(iNeuron, 3)));
+        elseif strcmp(neuronsGephi(iNeuron, 2),'Input')
+            fprintf(fileID,'%s,%s,%f,%f,%s,%s\n',string(neuronsGephi(iNeuron, 1)),string(neuronsGephi(iNeuron, 1)),...
+                0,input_posy(1),string(neuronsGephi(iNeuron, 2)),string(neuronsGephi(iNeuron, 3)));
             input_posy(1) = [];
-        elseif strcmp(neuronsGephi(i,2),'Readout')
-            if any(strcmp(readoutLayer{1,1},string(neuronsGephi(i,1))))
-                fprintf(fileID,'%s,%s,%f,%f,%s,%s\n',string(neuronsGephi(i,1)),string(neuronsGephi(i,1)),...
-                    20+normrnd(0,0.25),normrnd(0,1.5),...
-                    string(neuronsGephi(i,2)),string(neuronsGephi(i,3)));
+        elseif strcmp(neuronsGephi(iNeuron, 2),'Readout')
+            if any(strcmp(readoutLayer{1, 1},string(neuronsGephi(iNeuron, 1))))
+                fprintf(fileID,'%s,%s,%f,%f,%s,%s\n',string(neuronsGephi(iNeuron, 1)),string(neuronsGephi(iNeuron, 1)),...
+                    20+normrnd(0, 0.25),normrnd(0, 1.5),...
+                    string(neuronsGephi(iNeuron, 2)),string(neuronsGephi(iNeuron, 3)));
             else
-                fprintf(fileID,'%s,%s,%f,%f,%s,%s\n',string(neuronsGephi(i,1)),string(neuronsGephi(i,1)),...
+                fprintf(fileID,'%s,%s,%f,%f,%s,%s\n',string(neuronsGephi(iNeuron, 1)),string(neuronsGephi(iNeuron, 1)),...
                     16,readout_posy(1),...
-                    string(neuronsGephi(i,2)),string(neuronsGephi(i,3)));
+                    string(neuronsGephi(iNeuron, 2)),string(neuronsGephi(iNeuron, 3)));
                 readout_posy(1) = [];
             end
         end
     else
-        if strcmp(neuronsGephi(i,2),'Reservoir')
-            fprintf(fileID,'%s,%s,%f,%f,%s,%s\n',string(neuronsGephi(i,1)),'',...
-                8+normrnd(0,1.5),normrnd(0,1.5),string(neuronsGephi(i,2)),string(neuronsGephi(i,3)));
-        elseif strcmp(neuronsGephi(i,2),'Input')
-            fprintf(fileID,'%s,%s,%f,%f,%s,%s\n',string(neuronsGephi(i,1)),'',...
-                0,input_posy(1),string(neuronsGephi(i,2)),string(neuronsGephi(i,3)));
+        if strcmp(neuronsGephi(iNeuron, 2),'Reservoir')
+            fprintf(fileID,'%s,%s,%f,%f,%s,%s\n',string(neuronsGephi(iNeuron, 1)),'',...
+                8+normrnd(0, 1.5),normrnd(0, 1.5),string(neuronsGephi(iNeuron, 2)),string(neuronsGephi(iNeuron, 3)));
+        elseif strcmp(neuronsGephi(iNeuron, 2),'Input')
+            fprintf(fileID,'%s,%s,%f,%f,%s,%s\n',string(neuronsGephi(iNeuron,1)),'',...
+                0,input_posy(1),string(neuronsGephi(iNeuron, 2)),string(neuronsGephi(iNeuron, 3)));
             input_posy(1) = [];
-        elseif strcmp(neuronsGephi(i,2),'Readout')
-            if any(strcmp(readoutLayer{1,1},string(neuronsGephi(i,1))))
-                fprintf(fileID,'%s,%s,%f,%f,%s,%s\n',string(neuronsGephi(i,1)),'',...
-                    20+normrnd(0,0.25),normrnd(0,1.5),...
-                    string(neuronsGephi(i,2)),string(neuronsGephi(i,3)));
+        elseif strcmp(neuronsGephi(iNeuron, 2),'Readout')
+            if any(strcmp(readoutLayer{1, 1},string(neuronsGephi(iNeuron, 1))))
+                fprintf(fileID,'%s,%s,%f,%f,%s,%s\n',string(neuronsGephi(iNeuron, 1)),'',...
+                    20+normrnd(0, 0.25),normrnd(0, 1.5),...
+                    string(neuronsGephi(iNeuron, 2)),string(neuronsGephi(iNeuron, 3)));
             else
-                fprintf(fileID,'%s,%s,%f,%f,%s,%s\n',string(neuronsGephi(i,1)),'',...
+                fprintf(fileID,'%s,%s,%f,%f,%s,%s\n',string(neuronsGephi(iNeuron, 1)),'',...
                     16,readout_posy(1),...
-                    string(neuronsGephi(i,2)),string(neuronsGephi(i,3)));
+                    string(neuronsGephi(iNeuron, 2)),string(neuronsGephi(iNeuron, 3)));
                 readout_posy(1) = [];
             end
         end 
     end
 end
-
 fclose(fileID);
 
+% Store edges
 fileID = fopen('Gephi/edges.csv','w');
 fprintf(fileID,'Source, Target\n');
-
-for i = 1:length(neuronsGephi)
-    for j = 1:length(neuronsGephi)
-        if A(i,j)>0
-            fprintf(fileID,'%s,%s\n',string(neuronsGephi(i,1)),string(neuronsGephi(j,1)));
+for iNeuron = 1:nNeurons
+    for jNeuron = 1:nNeurons
+        if A(iNeuron, jNeuron)>0
+            fprintf(fileID,'%s,%s\n',string(neuronsGephi(iNeuron, 1)),string(neuronsGephi(jNeuron, 1)));
         end
     end
 end
-
 fclose(fileID);
--- Connectome/pruneNetwork.m
+++ Connectome/pruneNetwork.m
@@ -1,59 +1,52 @@
+% Clear command window and workspace
+clear;
+clc;
 
-clear;clc;
-addpath('Data/');
+% Add path to dependencies
+addpath 'Data/'
 
-%importing data from hermaphrodite chemical connectome
-
+% Import data from hermaphrodite chemical connectome
 A_table = readtable('herm_chemical.xlsx');
 
-%detecting pre and posynaptics neurons
-
+% Detect pre and posynaptics neurons
 posNeurons = table2cell(A_table(2,4:end-1))';
 preNeurons = table2cell(A_table(3:end-1,3));
-
 A = table2cell(A_table(3:end-1,4:end-1));
 
-for i = 1:length(preNeurons)
-    for j =1:length(posNeurons)
-        if strcmp(A(i,j),'')
-            A(i,j)={'0'};
+for iPreNeuron = 1:length(preNeurons)
+    for jPosNeuron =1:length(posNeurons)
+        if strcmp(A(iPreNeuron, jPosNeuron),'')
+            A(iPreNeuron, jPosNeuron)={'0'};
         end
     end
 end
 
 A = cellfun(@str2num, A);
 
-%making the network square
-
-[sharedNeurons,~,~] = intersect(posNeurons,preNeurons);
-    
-[notSharedNeurons,index] = setdiff(posNeurons,preNeurons);
-
+% Square the newtwork
+[sharedNeurons, ~, ~] = intersect(posNeurons,preNeurons);
+[notSharedNeurons, index] = setdiff(posNeurons,preNeurons);
 index = sort(index);
-
 neurons = posNeurons;
 
-for i = 1:length(index)
-    if index(i) <= size(A,1)
-        A = [A(1:index(i)-1,:); zeros(1,length(posNeurons)); A(index(i):end,:)];
+for iIndex = 1:length(index)
+    if index(iIndex) <= size(A,1)
+        A = [A(1:index(iIndex)-1,:); zeros(1,length(posNeurons)); A(index(iIndex):end,:)];
     else
         A = [A;zeros(1,length(posNeurons))];
     end    
 end
 
-%deleting neurons from pharynx
-
+% Deletee neurons from pharynx
 pharynxNeurons = 50;
 A(:,1:pharynxNeurons) = []; A(1:pharynxNeurons,:) = [];
 neurons(1:pharynxNeurons) = [];
 
 clearvars -except neurons A
 
-%filtering neurons not belonging either to input, reservoir or readout (inDegree=outDegree=0)
-
+% Filter neurons not belonging either to input, reservoir or readout (inDegree=outDegree=0)
 inDegree = sum(A,1);
 outDegree = sum(A,2);
-
 index = find(inDegree==0 & outDegree'==0);
 deletedNeurons = neurons(index);
 
@@ -63,17 +56,14 @@ neurons(index) = [];
 
 clearvars inDegree outDegree index
 
-%pruning
-
-%input layer detection
-
+% Pruning
+% Input layer detection
 A_pruned = A;
-
-inDegree = sum(A_pruned,1);
-
+inDegree = sum(A_pruned, 1);
 reservoirNeurons = neurons;
 layer = 1;
 inputLayer = {};
+
 while any(inDegree==0)
     index = find(inDegree==0);
     A_pruned(index,:) = []; A_pruned(:,index) = [];
@@ -85,10 +75,8 @@ end
 
 clearvars index inDegree layer
 
-%readout detection with layers
-
+% Readout detection with layers
 outDegree = sum(A_pruned,2);
-
 layer = 1;
 readoutLayer = {};
 
@@ -102,19 +90,17 @@ while any(outDegree==0)
 end
 
 A_norm_max = A_pruned;
-for i = 1:size(A_norm_max,1)
-   A_norm_max(i,:) = A_norm_max(i,:)/max(abs(A_pruned(i,:)));
+for iRow = 1:size(A_norm_max,1)
+   A_norm_max(iRow,:) = A_norm_max(iRow,:)/max(abs(A_pruned(iRow,:)));
 end
 
 clearvars layer outDegree index i
 
-save('Connectome/newConnectome_full.mat')
+% Uncomment to save
+% save('Connectome/newConnectome_full.mat')
 
 list_pruned = reservoirNeurons;
-
 clearvars -except A_pruned A_norm_max list_pruned
 
-save('Connectome/newConnectome_minimal.mat')
-
-
-
+% Uncomment to save
+% save('Connectome/newConnectome_minimal.mat')
